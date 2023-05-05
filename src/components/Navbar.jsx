9c7fe19b2cb18a098db9875b901d20d47569e10b
TheRitikDubey <www.ritikdubey@gmail.com>
2023-05-06 02:21:56 +0530

    Login page 40% done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,63 @@
-import {FaShoppingCart} from "react-icons/fa"
+import { FaShoppingCart } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-
-  const {cart} = useSelector((state) => state);
-
+  const { cart } = useSelector((state) => state);
+  const currentPath = window.location.pathname;
   return (
-    <div >
-      <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
+    <>
+      <div>
+        <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
+          <NavLink to="/">
+            <div className="ml-5">
+              <img src="../logo.png" className="h-14" />
+            </div>
+          </NavLink>
 
-        <NavLink to="/">
-          <div className="ml-5">
-          <img src="../logo.png" className="h-14"/>
-          </div>
-        </NavLink>
-
-          <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
+          {!currentPath.includes("") ? <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
             <NavLink to="/">
-              <p>Home</p>
+              <h2>Home</h2>
             </NavLink>
 
             <NavLink to="/cart">
               <div className="relative">
-                  <FaShoppingCart className="text-2xl"/>
-                  {
-                    cart.length > 0 &&
-                    <span
+                <FaShoppingCart className="text-2xl" />
+                {cart.length > 0 && (
+                  <span
                     className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex 
-                    justify-center items-center animate-bounce rounded-full text-white" 
-                    >{cart.length}</span>
-                  }
-                  
+                    justify-center items-center animate-bounce rounded-full text-white"
+                  >
+                    {cart.length}
+                  </span>
+                )}
               </div>
             </NavLink>
-            
-          </div>
-      </nav>
-    </div>
-  )
+          </div> : <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
+            <NavLink to="/">
+              <h2>Login to ECOMZY</h2>
+            </NavLink>
+
+            {/* <NavLink to="/cart">
+              <div className="relative">
+                <FaShoppingCart className="text-2xl" />
+                {cart.length > 0 && (
+                  <span
+                    className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex 
+                    justify-center items-center animate-bounce rounded-full text-white"
+                  >
+                    {cart.length}
+                  </span>
+                )}
+              </div>
+            </NavLink> */}
+          </div>}
+
+          
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
